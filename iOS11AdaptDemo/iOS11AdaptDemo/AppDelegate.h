d44b7bf8c7317406151a5e076167599ddfe0f921
DeLongYang <1157013577@qq.com>
2017-10-18 15:02:17 +0800

    1.0  初始化 工程

---
+++ iOS11AdaptDemo/iOS11AdaptDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS11AdaptDemo
+//
+//  Created by DeLongYang on 2017/10/18.
+//  Copyright © 2017年 DeLongYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
